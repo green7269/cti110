81e92e668f6e54fb7aefea627a4b32d1baa4fd55
green7269 <greenf7269@student.faytechcc.edu>
2025-03-31 09:59:41 -0400

    Add files via upload
    First submission.

---
+++ P1LAB1_GreenFaith.py
@@ -0,0 +1,14 @@
+# Faith Green 
+# 03/31/2025
+# Assignment Name: P1LAB1
+# This program prompts the user to enter their first and last names separately 
+# and then displays a welcome message using those names.
+
+# Prompt the user to enter their first name
+first_name = input("Please enter your first name:Faith ")
+
+# Prompt the user to enter their last name
+last_name = input("Please enter your last name:Green ")
+
+# Display a welcome message using the user's first and last name
+print(f"Welcome to CTI 110 course, Faith Green!")
